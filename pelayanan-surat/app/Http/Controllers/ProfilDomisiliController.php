5af190a1329533d7f760fafa33fe26f77fa7f283
Anwarru1 <anwaruromadon@gmail.com>
2025-08-24 22:46:42 +0700

    fix

--- pelayanan-surat/app/Http/Controllers/ProfilDomisiliController.php
+++ pelayanan-surat/app/Http/Controllers/ProfilDomisiliController.php
@@ -80,7 +80,7 @@ class ProfilDomisiliController extends Controller
                 $file = $request->file($field);
                 $filename = $field . '-' . Str::random(10) . '.' . $file->getClientOriginalExtension();
 
-                $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/storage/domisili/syarat-verifikasi/';
+                $destinationPath = public_path('storage/domisili/syarat-verifikasi/');
                 if (!file_exists($destinationPath)) {
                     mkdir($destinationPath, 0755, true);
                 }
